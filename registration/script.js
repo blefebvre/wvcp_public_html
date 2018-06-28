b6e45a5b437e7eea65b6052324eec89191164765
Bruce Lefebvre <brucelefebvre@gmail.com>
2018-06-27 22:40:13 -0400

    Registration page: address email feedback

--- registration/script.js
+++ registration/script.js
@@ -13,19 +13,21 @@
 
         // Enable checkbox toggle
         var icon = step.querySelector(".icon");
-        icon.addEventListener("click", function () {
+        icon.addEventListener("click", function (event) {
             var checked = (step.dataset.complete === "false" ? "true" : "false");
             step.dataset.complete = checked;
             localStorage.setItem(LS_CHECKED_PREFIX + index, checked);
+            event.preventDefault();
         }, false);
 
         // Enable detail to be toggled
         var detailLink = step.querySelector(".show-detail");
         var detailDiv = step.querySelector(".step-details");
         if (detailLink != null) {
-            detailLink.addEventListener("click", function () {
+            detailLink.addEventListener("click", function (event) {
                 var showingDetails = (step.dataset.showingDetails === "false" ? "true" : "false");
                 step.dataset.showingDetails = showingDetails;
+                event.preventDefault();
             }, false);
         }
     });
